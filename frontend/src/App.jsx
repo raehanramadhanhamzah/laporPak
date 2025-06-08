6bc4556c9e0c2999aaa0b7a607e195cebeae5266
Raehanrh <113957089+raehanramadhanhamzah@users.noreply.github.com>
2025-06-08 23:18:14 +0700

    FIX: reset folder frontned

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./routes/PrivateRoute";
-import Navbar from "./components/Navbar";
-import Login from "./pages/login/LoginPresenter";
-import Register from "./pages/register/RegisterPresenter";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Homepage from "./pages/homepage/Homepage";
-
-import StandardReportForm from "./pages/report-form/StandardFormPresenter";
-import QuickReportForm from "./pages/report-form/QuickFormPresenter";
-
-import About from "./pages/about/About";
-import Statistics from "./pages/statistics/Statistics";
-
-import "leaflet/dist/leaflet.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/statistics" element={<Statistics />} />
-        
-        <Route path="/reports/quick" element={<QuickReportForm />} />
-        <Route path="/reports/standard" element={<StandardReportForm />} />
-        
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/reports/quick/protected" element={<QuickReportForm />} />
-          <Route path="/reports/standard/protected" element={<StandardReportForm />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
